f4bfe6f2e8970163e8feb0a5e828971472d91400
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-08-01 22:49:28 +0500

    .

---
+++ js/script.js
@@ -0,0 +1,50 @@
+//forEach metodiga oid masalalar
+//1-masala
+// const numbers = [1, 2, 3, 4];
+// function multiplyTwoElement(arg) {
+//   let res = [];
+//   arg.forEach((element) => {
+//     res.push(element * 2);
+//   });
+//   return res;
+// }
+// console.log(multiplyTwoElement(numbers));// 2, 4, 6, 8
+
+//2-masala
+// const words = ["apple", "banana", "cherry"];
+// function toUpperCaseElement(arg) {
+//   let res = [];
+//   arg.forEach((element) => {
+//     res.push(element.toUpperCase());
+//   });
+//   return res;
+// }
+// console.log(toUpperCaseElement(words)); // 'APPLE', 'BANANA', 'CHERRY'
+
+//3-masala
+// const numbers = [10, 20, 30, 40];
+// function addFiveElement(arg) {
+//   let res = [];
+//   arg.forEach((element) => {
+//     res.push(element + 5);
+//   });
+//   return res;
+// }
+// console.log(addFiveElement(numbers));
+
+//4-masala
+// const students = [
+//   { name: "Ali", grade: 85 },
+//   { name: "Vali", grade: 92 },
+//   { name: "Hasan", grade: 78 },
+// ];
+// function nameObject(arg) {
+//   let res = [];
+//   arg.forEach((element) => {
+//     res.push(element);
+//   });
+//   return res;
+// }
+// console.log(nameObject(students));
+
+//Map metodiga oid masalalar
